e4c3d41e61fc7a4934d665e380349e58ae87221d
kabdik <87753859226.ansar@gmail.com>
2022-12-22 23:05:45 +0600

    adding gitignore and starting jwt authorization

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from 'src/users/dto/createUser.dto';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private userService:UsersService,
+        ){}
+
+    async login(userDto:CreateUserDto){
+
+    }
+    
+    async registration(userDto:CreateUserDto){
+        
+    }
+}
